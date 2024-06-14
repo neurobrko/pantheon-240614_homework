8a9c2d93590d0eb1a0146885802127ee880630bf
neurobrko <elvis@elvis.sk>
2024-06-15 00:46:44 +0200

    002 - Part A complete

--- ugen.py
+++ ugen.py
@@ -2,9 +2,10 @@
 
 import argparse
 import sys
+import pandas as pd
 
 
-# Create cusotm argument parser to be able to alter error message
+# Create custom argument parser to be able to alter error message
 class CustomArgumentParser(argparse.ArgumentParser):
     # redefine error message to print usage along with the error
     def error(self, message):
@@ -35,4 +36,57 @@ ap.add_argument(
 
 # retrieve arguments
 args = ap.parse_args()
-print(args)
+
+input_data = []
+# load input files into list of lists and strip lines of \n or eventual whitespaces
+for file in args.inputFiles:
+    for line in file:
+        line = line.strip()
+        line = line.split(":")
+        input_data.append(line)
+    file.close()
+
+# create dataframe populated with input_data
+column_names = ["ID", "Name", "Middle Name", "Surname", "Department"]
+input_df = pd.DataFrame(input_data, columns=column_names)
+
+# create Username column
+input_df.insert(
+    1,
+    "Username",
+    input_df["Name"].apply(lambda s: s[0].lower())
+    + input_df["Middle Name"].apply(lambda s: s[0].lower() if len(s) > 0 else "")
+    + input_df["Surname"].apply(lambda s: s.lower()),
+)
+# strip Username to 8 characters
+input_df["Username"] = input_df["Username"].apply(lambda s: s[:8])
+# check for duplicate usernames and add number to them
+
+
+# custom function to add a number at the end of username
+def add_number(username, counts):
+    if username in counts:
+        counts[username] += 1
+        return f"{username}{counts[username]}"
+    else:
+        counts[username] = 0
+        return username
+
+
+# empty dict for counts for each username
+username_counts = {}
+
+# find duplicates and apply the custom function
+input_df["is_duplicate"] = input_df.duplicated(subset="Username", keep=False)
+input_df["Username"] = input_df.apply(
+    lambda x: (
+        add_number(x["Username"], username_counts)
+        if x["is_duplicate"]
+        else x["Username"]
+    ),
+    axis=1,
+)
+input_df.drop("is_duplicate", axis=1, inplace=True)
+
+# write input_df to outuput.txt
+input_df.to_csv("output_file.txt", header=False, index=False, sep=":")
