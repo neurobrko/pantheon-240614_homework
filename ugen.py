f2d63e44a76cae0995579082d46d6ba09f9316c8
neurobrko <elvis@elvis.sk>
2024-06-15 12:05:54 +0200

    003 - Added data validation and report

--- ugen.py
+++ ugen.py
@@ -13,6 +13,13 @@ class CustomArgumentParser(argparse.ArgumentParser):
         self.print_help()
         sys.exit(2)
 
+# create class for formatting report output
+class FormatCli:
+   GREEN = '\033[92m'
+   YELLOW = '\033[93m'
+   RED = '\033[91m'
+   BOLD = '\033[1m'
+   END = '\033[0m'
 
 # Create argument parser
 ap = CustomArgumentParser(
@@ -37,13 +44,62 @@ ap.add_argument(
 # retrieve arguments
 args = ap.parse_args()
 
+# create list for input data
 input_data = []
-# load input files into list of lists and strip lines of \n or eventual whitespaces
+# create list for wrong formatted lines
+wrong_format = []
+# create vars for processed lines and skipped lines
+processed_lines = 0
+skipped_lines = 0
+
+# load input files into list of lists
 for file in args.inputFiles:
+    i = 1
     for line in file:
+        # strip lines of \n or eventual whitespaces
         line = line.strip()
+        # spilt line into list
         line = line.split(":")
+
+        # check if the separator was correct and there is 5 values in list
+        if len(line) != 5:
+            wrong_format.append(
+                f"{FormatCli.BOLD}{FormatCli.RED}ERR:{FormatCli.END} {file.name}, line {i}: Wrong format of data. "
+                f"{FormatCli.BOLD}{FormatCli.RED}Line skipped!{FormatCli.END}")
+            skipped_lines += 1
+            i += 1
+            continue
+
+        # check if ID is int
+        try:
+            int(line[0])
+        except ValueError:
+            wrong_format.append(
+                f"{FormatCli.BOLD}{FormatCli.RED}ERR:{FormatCli.END} {file.name}, line {i}: "
+                f"ID is not a number. {FormatCli.BOLD}{FormatCli.RED}Line skipped!{FormatCli.END}")
+            skipped_lines += 1
+            i += 1
+            continue
+
+        # check if Forename or Surname are not empty
+        if line[1] == "" or line[3] == "":
+            wrong_format.append(
+                f"{FormatCli.BOLD}{FormatCli.RED}ERR:{FormatCli.END} {file.name}, line {i}: "
+                f"Forename or Surname is empty. {FormatCli.BOLD}{FormatCli.RED}Line skipped!{FormatCli.END}")
+            skipped_lines += 1
+            i += 1
+            continue
+
+        # check if department is empty
+        if line[4] == "":
+            wrong_format.append(
+                f"{FormatCli.BOLD}{FormatCli.YELLOW}WARN:{FormatCli.END} {file.name}, line {i}: "
+                f"Department is not specified.")
+
         input_data.append(line)
+        processed_lines += 1
+        i += 1
+
     file.close()
 
 # create dataframe populated with input_data
@@ -90,3 +146,13 @@ input_df.drop("is_duplicate", axis=1, inplace=True)
 
 # write input_df to outuput.txt
 input_df.to_csv("output_file.txt", header=False, index=False, sep=":")
+
+# print report
+print(f"{FormatCli.GREEN}{FormatCli.BOLD}{processed_lines} lines processed.{FormatCli.END}")
+if skipped_lines == 0:
+    print(f"{FormatCli.BOLD}{skipped_lines} lines skipped.{FormatCli.END}")
+else:
+    print(f"{FormatCli.RED}{FormatCli.BOLD}{skipped_lines} lines skipped.{FormatCli.END}")
+if wrong_format:
+    print(f"\n{FormatCli.RED}{FormatCli.BOLD}Errors and warnings:{FormatCli.END}")
+    print(*wrong_format, sep="\n")
